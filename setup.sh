e2bdfa4b71d0d5a3623565eff93feb0cf088efb7
allogic <mial.burg@gmail.com>
2019-09-17 15:23:12 +0200

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 vendor=$(pwd)/engine/vendor
+mkdir -p $vendor
 
-mkdir -p $vendor/glad && cd $vendor/glad
+git clone https://github.com/Dav1dde/glad $vendor/glad
+cd $vendor/glad
 python -m glad --generator=c --out-path=gl
 
-mkdir -p $vendor/glfw && cd $vendor/glfw
+git clone https://github.com/glfw/glfw $vendor/glfw
+cd $vendor/glfw
 cmake .
 
-mkdir -p $vendor/assimp && cd $vendor/assimp
+git clone https://github.com/g-truc/glm $vendor/glm
+
+git clone https://github.com/assimp/assimp $vendor/assimp
+cd $vendor/assimp
 cmake . && make -j4
